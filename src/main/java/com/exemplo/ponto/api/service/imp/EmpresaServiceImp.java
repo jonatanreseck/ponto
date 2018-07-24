15b9d6075b6dbafc2f3754767142b5f095414bb8
Jonatan <jonatan.reseck@leucotron.com.br>
2018-07-24 10:18:46 -0300

    add empresa e funcionario service com os testes

---
+++ src/main/java/com/exemplo/ponto/api/service/imp/EmpresaServiceImp.java
@@ -0,0 +1,35 @@
+package com.exemplo.ponto.api.service.imp;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exemplo.ponto.api.entities.Empresa;
+import com.exemplo.ponto.api.repositories.EmpresaRepository;
+import com.exemplo.ponto.api.service.EmpresaService;
+
+@Service
+public class EmpresaServiceImp implements EmpresaService {
+	
+
+	private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImp.class);
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	@Override
+	public Optional<Empresa> buscarPorCnpj(String cnpj) {
+		log.info("Buscando uma empresa para o CNPJ {}", cnpj);
+		return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+	}
+
+	@Override
+	public Empresa persistir(Empresa empresa) {
+		log.info("Persistindo empresa: {}", empresa);
+		return this.empresaRepository.save(empresa);
+	}
+
+}
