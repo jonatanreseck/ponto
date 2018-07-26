bc0b7f10c12d224b10af718f4b2c1ac8f2098c33
Jonatan <jonatan.reseck@leucotron.com.br>
2018-07-26 10:31:01 -0300

    Add security controller / lancamento controller / empresa Controller

---
+++ src/main/java/com/exemplo/ponto/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.exemplo.ponto.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.exemplo.ponto.api.entities.Funcionario;
+import com.exemplo.ponto.api.security.JwtUserFactory;
+import com.exemplo.ponto.api.service.FuncionarioService;
+
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
